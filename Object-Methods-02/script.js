be354959be1356660139d30b88b19e3fa40a0d54
Mst <bilkis.naher@yahoo.co.uk>
2024-10-23 12:04:20 +0100

    add html and js files

---
+++ Object-Methods-02/script.js
@@ -0,0 +1,14 @@
+alert('test')
+
+
+var chosenPet = 'Lulu';
+
+var shelter = {
+    dogs: ['Mackie', 'Bernice', 'Coolie Monster', 'Spot'],
+    cats: ['Astrid', 'Lulu', 'Fluffy', 'Mouser'],
+    apptMessage: function () {
+        console.log(
+            'Emain us at meetafriend@wayadpets.com to make an appointmemt to meet your new friend.'
+        );
+    },
+};
