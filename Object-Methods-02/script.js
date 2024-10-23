3a0b3f52687490c07261256393022c10d27d7998
Mst <bilkis.naher@yahoo.co.uk>
2024-10-23 21:30:31 +0100

    add logic to the code base

--- Object-Methods-02/script.js
+++ Object-Methods-02/script.js
@@ -17,14 +17,27 @@ var shelter = {
 
 function dogMessage() {
     console.log(
-        'Congrats!' + ChosenPet + ', a great dog , is available for adoption!'
+        'Congrats!' + chosenPet + ', a great dog , is available for adoption!'
     );
     shelter.apptMessage();
 }
 
 function catMessage() {
     console.log(
-        'Congrats!' + ChosenPet + ', a great cat , is available for adoption!'
+        'Congrats!' + chosenPet + ', a great cat , is available for adoption!'
     );
     shelter.apptMessage();
 }
+
+if(shelter.dogs.includes(chosenPet)){
+       dogMessage();
+}else if(shelter.cats.includes(chosenPet)){
+        catMessage();
+}else {
+    console.log('Its look like pet animel is not available' );
+    console.log('Checkout our featured dog' + 
+        shelter.dog[3] + ', checkout our featured cat' +
+        shelter.cat[1]
+    );
+}
+
